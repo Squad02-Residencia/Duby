ce6263b5a5140e477c60c62605fd39649a4ef39d
Lorena Mariah Oliveira Lima <168576064+Lorena2423@users.noreply.github.com>
2025-05-28 19:23:20 -0300

    Subindo projeto com backend e Página de Cadastro de Adquirente

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',  // seu nome do MySQL
-  password: 'Gatao-04', // sua senha do MySQL
+  password: 'sua senha', // sua senha do MySQL
   database: 'dubydb'
 });
 
