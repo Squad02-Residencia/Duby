6bf513fd1e618db70bdb2017dc527d6309aa55a5
AndersonCodhugo <anderson_hindo@hotmail.com>
2025-06-02 19:48:23 -0300

    fix 2

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',  // seu nome do MySQL
-  password: 'Banco2020@', // sua senha do MySQL
+  password: 'Gatao-04', // sua senha do MySQL
   database: 'dubydb'
 });
 
